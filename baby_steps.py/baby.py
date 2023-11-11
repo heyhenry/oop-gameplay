48dd4c4b554e6e97be86b00d4598fd6f3353e1b1
heyhenry <henryyy3@hotmail.com>
2023-11-11 11:44:55 +0800

    Initial commit

---
+++ baby_steps.py/baby.py
@@ -0,0 +1,21 @@
+class Baby:
+
+    # class attributes/variables
+    action_1 = 'roll over'
+    action_2 = 'crawl'
+
+    # instance of attributes/variables
+    def __init__(self, name):
+        self.name = name
+
+    def first_step(self):
+        print('Baby ' + self.name + ' has just performed the skill: ' + self.action_1)
+
+    def second_step(self):
+        print('Baby ' + self.name + ' has just performed the skill: ' + self.action_2)
+    
+Ariel = Baby('Ariel')
+Paing = Baby('Paing')
+
+Ariel.first_step()
+Paing.second_step()
